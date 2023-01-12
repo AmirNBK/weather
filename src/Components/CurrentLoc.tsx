9265a26b3aee1ff0e02646142804e27248ea6f85
AmirNBK <80398198+AmirNBK@users.noreply.github.com>
2023-01-12 04:21:20 +0330

    added the weather description code

--- src/Components/CurrentLoc.tsx
+++ src/Components/CurrentLoc.tsx
@@ -8,19 +8,25 @@ const Current = () => {
     const [lat, setLat] = useState("")
     const [long, setLong] = useState("")
     const [temp, setTemp] = useState("")
-    const [city,setCity] = useState("")
+    const [city, setCity] = useState("")
+    const [weatherCode, setWeatherCode] = useState("")
+    const [description, setDescription] = useState("")
 
     useEffect(() => {
         getCity()
+        getDescription()
     }, [])
 
     const getPosition = (position) => {
         setLat(position.coords.latitude)
         setLong(position.coords.longitude)
         axios
-            .get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m&current_weather=true`)
+            .get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m&current_weather=true&daily=apparent_temperature_max`)
             .then((response) => {
+                // console.log(response.data)
                 setTemp(response.data.current_weather.temperature)
+                setWeatherCode(response.data.current_weather.weathercode)
+                console.log(weatherCode)
             })
             .catch((error) => {
                 // handle error
@@ -29,6 +35,7 @@ const Current = () => {
                 // always executed
             })
 
+        getDescription()
     }
 
     window.navigator.geolocation
@@ -38,7 +45,6 @@ const Current = () => {
         axios
             .get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en`)
             .then((response) => {
-                console.log(response.data)
                 setCity(response.data.city)
             })
             .catch((error) => {
@@ -49,6 +55,47 @@ const Current = () => {
             })
     }
 
+    const getDescription = () => {
+        if (weatherCode == "0") {
+            setDescription("Clear sky")
+        }
+        if (weatherCode == "1" || weatherCode == "2" || weatherCode == "3") {
+            setDescription("Mainly clear, partly cloudy, and overcast")
+        }
+        if (weatherCode == "45" || weatherCode == "58") {
+            setDescription("Fog and depositing rime fog")
+        }
+        if (weatherCode == "51" || weatherCode == "53" || weatherCode == "58") {
+            setDescription("Drizzle: Light, moderate, and dense intensity")
+        }
+        if (weatherCode == "56" || weatherCode == "57") {
+            setDescription("Freezing Drizzle: Light and dense intensity")
+        }
+        if (weatherCode == "61" || weatherCode == "63" || weatherCode == "65") {
+            setDescription("Rain: Slight, moderate and heavy intensity")
+        }
+        if (weatherCode == "66" || weatherCode == "67") {
+            setDescription("Freezing Rain: Light and heavy intensity")
+        }
+        if (weatherCode == "71" || weatherCode == "73" || weatherCode == "75") {
+            setDescription("Snow fall: Slight, moderate, and heavy intensity")
+        }
+        if (weatherCode == "77") {
+            setDescription("Snow grains")
+        }
+        if (weatherCode == "80" || weatherCode == "81" || weatherCode == "82") {
+            setDescription("Rain showers: Slight, moderate, and violent")
+        }
+        if (weatherCode == "85" || weatherCode == "86") {
+            setDescription("Snow showers slight and heavy")
+        }
+        if (weatherCode == "95") {
+            setDescription("Thunderstorm: Slight or moderate")
+        }
+        if (weatherCode == "96" || weatherCode == "99") {
+            setDescription("Thunderstorm with slight and heavy hail")
+        }
+    }
 
     return (
         <div className='CurrentLocMainContainer'>
@@ -59,7 +106,7 @@ const Current = () => {
                 <div className='CurrentLocMainContainer__tempRange__left'> 6° </div>
                 <div className='CurrentLocMainContainer__tempRange__right'> 22° </div>
             </div>
-            <div className='CurrentLocMainContainer__status'> Scattered thundershowers </div>
+            <div className='CurrentLocMainContainer__status'> {description} </div>
         </div>
     )
 }
